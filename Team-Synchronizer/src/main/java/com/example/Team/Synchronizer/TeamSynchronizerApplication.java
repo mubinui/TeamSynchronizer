7b5aa6c7cba526bcdbe41962d491b55e50df3707
uicmu <mubin41@student.sust.edu>
2021-09-28 14:32:44 +0600

    First Commit -> Team Synchronizer

---
+++ Team-Synchronizer/src/main/java/com/example/Team/Synchronizer/TeamSynchronizerApplication.java
@@ -0,0 +1,14 @@
+package com.example.Team.Synchronizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSynchronizerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TeamSynchronizerApplication.class, args);
+	}
+
+}
---
+++ Team-Synchronizer/src/test/java/com/example/Team/Synchronizer/TeamSynchronizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Team.Synchronizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamSynchronizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
